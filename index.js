07f00d2054e8e922b31918bf36bb6cbc151769ee
Your Name <princesslorna2000@gmail.com>
2024-07-05 22:58:14 +0300

    using filter

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+function findMatching(drivers, searchString) {
+   
+    const searchLowerCase = searchString.toLowerCase();
+
+     const matchingDrivers = drivers.filter(driver =>
+        driver.toLowerCase().includes(searchLowerCase)
+    );
+
+    return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, query) {
+    
+    const queryLowerCase = query.toLowerCase();
+
+    
+    const matchingDrivers = drivers.filter(driver =>
+        driver.toLowerCase().startsWith(queryLowerCase)
+    );
+
+    return matchingDrivers;
+}
+
+function matchName(drivers, name) {
+    
+    const matchingDrivers = drivers.filter(driver =>
+        driver.name.toLowerCase() === name.toLowerCase()
+    );
+
+    return matchingDrivers;
+}
